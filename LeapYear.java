62835d0b2cb348bba2f8ab6d5030f00c5da4866a
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2019-08-09 12:58:21 +0400

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -7,20 +7,20 @@ if(year%4==0){
 if(year%100==0){
 if(year%400==0)
 {
-  System.out.println(year+" is leap");
-}
-else{ 
-  System.out.println(" not");
-}
-}
-else
-{
   System.out.println("leap");
 }
+else{ 
+  System.out.println("no");
+}
 }
 else
 {
-  System.out.println(year+" is not");
+  System.out.println("Yes");
+}
+}
+else
+{
+  System.out.println("no");
 }
 }
 }
