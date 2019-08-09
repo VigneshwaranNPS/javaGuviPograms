3b42e74bd7035f37cfef84ddd0ec79ae20a76316
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2019-08-09 12:41:36 +0400

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -5,10 +5,13 @@ Scanner Scan=new Scanner(System.in);
 System.out.println("enter a choose");
 char ch=Scan.next().charAt(0);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-  System.out.println(ch+"is vowel");
+  System.out.println("vowel");
 }
 else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='g'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z'){
-  System.out.println(ch+"is consonant");
+  System.out.println("consonant");
 }
+else{
+  System.out.println("Invalid");
+  
 }
 }
