967bbf38993abc73a24e484b42c715d6ac19f1d0
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2019-08-09 11:57:39 +0400

    Create Oddoreven.java

---
+++ Oddoreven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Oddoreven{
+public static void main(String[] args){
+System.out.println("Enter the Number");
+Scanner x=new Scanner(System.in);
+int a=x.nextInt();
+if (a%2==0){
+  System.out.println("The number is even");
+}
+else{
+  System.out.println("The number is odd");
+}
+}
+}
