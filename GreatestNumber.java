bbd1d0d919b68217f4bd9e79864a40ecc13a159c
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2019-08-09 12:51:57 +0400

    Create GreatestNumber.java

---
+++ GreatestNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class GreatestNumber{
+public static void main(String[] qwe){
+Scanner Scan=new Scanner(System.in);
+int a=Scan.nextInt();
+int b=Scan.nextInt();
+int c=Scan.nextInt();
+if(a>b&&b>c){
+System.out.println(a);
+}
+else if(b>c){
+  System.out.println(b);
+}
+else
+{
+  System.out.println(c);
+}
+}
+}
