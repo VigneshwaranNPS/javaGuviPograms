4c14c55688b9c113fd630969b1986b499930641a
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2019-08-09 12:39:52 +0400

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -7,7 +7,7 @@ char ch=Scan.next().charAt(0);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
   System.out.println(ch+"is vowel");
 }
-else{
+else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='g'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z'){
   System.out.println(ch+"is consonant");
 }
 }
