96de92772a34778ac6d1b3c2abce45c8877ec535
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2021-10-19 18:57:32 +0530

    Add files via upload

---
+++ InOut.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InOut{
+	public static void main(String [] args) {
+		int i;
+		Scanner in=new Scanner(System.in);
+		i=in.nextInt();
+		System.out.println(i);
+	}
+}
