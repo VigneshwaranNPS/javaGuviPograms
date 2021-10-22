f2e629b924349d8a157c2bc6b6ee5f61f80dd138
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2021-10-22 19:55:25 +0530

    StringIN.java

---
+++ StringIN.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StringIN{
+	public static void main(String [] args){
+		Scanner Ui=new Scanner(System.in);
+		String Given =Ui.nextLine();
+		System.out.print(Given);
+	}
+}
