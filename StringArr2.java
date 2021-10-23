eab180316835437c77ec3843388fe2523b9ec3ef
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2021-10-23 18:37:21 +0530

    StringArr2.java

--- StringArr2.java
+++ StringArr2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class Main{
-	@SuppressWarnings({ "resource" })
 	public static void main(String []args){
 		Scanner v=new Scanner(System.in);
 		int n=v.nextInt(),k=v.nextInt();
 		int[] Arr=new int[n];
-		System.out.println(n+" "+k);
 		for(int i=0;i<n;i++) {
 			Arr[i]=v.nextInt();
-			System.out.print(Arr[i]+" ");
 		}	
+		System.out.println(n+" "+k);
+		for(int i=0;i<n;i++) {
+			System.out.print(Arr[i]+" ");
+		}
 	}
 }	
