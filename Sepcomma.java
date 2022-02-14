b9209299acd7c5868648509acdc937650e0851ec
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-14 20:36:17 +0530

    Sepcomma.java

--- Sepcomma.java
+++ Sepcomma.java
@@ -5,6 +5,11 @@ public class Sepcomma{
 		Scanner read_in=new Scanner(System.in);
 		String given_in=read_in.next();
 		char taken_in[]=given_in.toCharArray();
+		String ref="0,1,2,3,4,5,6,7,8,9";
+		if(given_in.contains(ref)) {
+			System.out.print("Invalid");
+		}
+		else {
 		for(int i=0;i<taken_in.length;i++) {
 			System.out.print(taken_in[i]);
 			if(i==taken_in.length-1) {
@@ -12,5 +17,6 @@ public class Sepcomma{
 			}
 			System.out.print(",");
 		}
+	}
 	}
 }
