1a984b4b4b3f522abf6bb655e45035c7f95f827a
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-16 04:02:43 +0530

    Update Oddoreven.java

--- Oddoreven.java
+++ Oddoreven.java
@@ -5,13 +5,13 @@ System.out.println("Enter the Number");
 Scanner x=new Scanner(System.in);
 int a=x.nextInt();
 if (a%2==0){
-  System.out.println("The number is even");
+  System.out.println("even");
 }
 else if(a%2<0){
   System.out.println("Invalid");
 }
 else{
-  System.out.println("The number is odd");
+  System.out.println("odd");
 }
 }
 }
