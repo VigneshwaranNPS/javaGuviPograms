1cd9a4ec053a63654122c2debd62c3b1028f4578
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-16 04:35:09 +0530

    Create StringSep.java

---
+++ StringSep.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+	public static void main(String args[]) {
+		Scanner st=new Scanner(System.in);
+		String ent=st.next();
+		char entchar[]=ent.toCharArray();
+		for(int i=0;i<ent.length();i++) {
+			System.out.println(entchar[i]);
+		}
+	}
+}
