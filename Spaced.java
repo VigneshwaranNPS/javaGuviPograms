94284f74531245f4b5ea3bfc24685a4821c20c2e
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-16 19:48:07 +0530

    Create Spaced.java

---
+++ Spaced.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Scanner jk= new Scanner(System.in);
+		String h=jk.next();
+		char strr[]=h.toCharArray();
+		for(int i=0;i<strr.length;i++) {
+			System.out.print(strr[i]+" ");
+		}
+	}
+}
