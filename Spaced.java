45fbb103e6bbfd4242301fc02295145ab535d49b
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-16 21:27:34 +0530

    Update Spaced.java

--- Spaced.java
+++ Spaced.java
@@ -4,8 +4,14 @@ class Main{
 		Scanner jk= new Scanner(System.in);
 		String h=jk.next();
 		char strr[]=h.toCharArray();
+		StringBuilder strrin = new StringBuilder("");
 		for(int i=0;i<strr.length;i++) {
-			System.out.print(strr[i]+" ");
+			strrin.append(strr[i]);
+			if(i==strr.length-1) {
+				break;
+			}
+			strrin.append(" ");
 		}
+		System.out.print(strrin);
 	}
 }
