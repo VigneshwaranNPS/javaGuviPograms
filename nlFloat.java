0c2928ff9be38a65ad3777a218e3e59616f1a8ca
VigneshwaranNPS <53895740+VigneshwaranNPS@users.noreply.github.com>
2022-02-16 19:25:40 +0530

    Create nlFloat.java

---
+++ nlFloat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+	public static void main(String args[]) {
+		Scanner hj=new Scanner(System.in);
+		float a,b,c;
+		a=hj.nextFloat();
+		b=hj.nextFloat();
+		c=hj.nextFloat();
+		System.out.print(a+"\n"+b+"\n"+c);
+	}
+}
